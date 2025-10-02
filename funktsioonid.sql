3976a1e7906a58e2297851b8159fb0ea64e67ca8
Irina Merkulova <irina.merkulova@gmail.com>
2025-10-02 16:27:08 +0300

    inlinefunction on lisatud

---
+++ funktsioonid.sql
@@ -0,0 +1,12 @@
+--funktsioonid
+SElect * from DimEmployee
+
+--Tabelisisev‰‰rtusega funktsioon e Inline Table Valued function (ILTVF)
+CREATE FUNCTION fn_ILTVF_GetEmployees()
+Returns Table
+as
+Return (Select EmployeeKey, FirstName, 
+Cast(BirthDate as Date) as DOB
+From dbo.DimEmployee);
+--k‰ivita funktsiooni
+SELECT * FROM fn_ILTVF_GetEmployees();
